701334c2ef632038c0dce21bcb65b29df3dce2dc
alisia142 <alisiageerts@hotmail.com>
2019-01-09 12:38:46 +0100

    Domains toevoegen

--- resources/views/users/domains/create.blade.php
+++ resources/views/users/domains/create.blade.php
@@ -5,12 +5,16 @@
     {{--Add new domains--}}
     <div class="col-sm6">
 
-        {!! Form::open(['method'=>'POST', 'action'=>['UserDomainController@store']]) !!}
+        {!! Form::open(['method'=>'POST', 'action'=>['UserDomainController@store', $user->id]]) !!}
         @csrf
         <div class="form-group">
             {!! Form::label('urlname', 'Domain:') !!}
             {!! Form::text('urlname', null, ['class'=>'form-control']) !!}
         </div>
+        <div class="form-group">
+            {!! Form::hidden('user_id', $user->id) !!}
+        </div>
+        {!! Form::hidden($value = $userId) !!}
         <div class="form-group">
             {!! Form::submit('Add Domain', ['class'=>'btn btn-primary']) !!}
         </div>
