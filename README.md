7037238f4f4fe5ecb74969b434fd1b4c8e4a4dfd
Unknown <m.koning02@student.alfa-college.nl>
2019-01-16 12:26:04 +0100

    apikeys table added

--- README.md
+++ README.md
@@ -3,7 +3,4 @@ composer require "webpatser/laravel-uuid:^3.0"
 
 ##  Smol to-do list
 -API <br>
--Edit <br>
 -Send signal to server that domain name is verified <br>
--Upon editing domain name reset verified back to unverified <br>
--Delete domains after set time if not verified? <br>
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,14 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function prot_api()
+    {
+        $data = [
+            'name' => 'Henk',
+            'email' => 'henk@live.nl'
+        ];
+
+        return response()->json($data);
+    }
 }
