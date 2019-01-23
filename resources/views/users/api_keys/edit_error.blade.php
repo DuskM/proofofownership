b9b86b1c71efda99199dbeab1bf139a445f98abd
alisia142 <alisiageerts@hotmail.com>
2019-01-23 12:55:04 +0100

    changed error message

--- resources/views/users/api_keys/edit_error.blade.php
+++ resources/views/users/api_keys/edit_error.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <h4>You can't use the same API Key label!</h4>
+    <h4>This API Label is already in use!</h4>
     <h5>Click <a href="/domain/{{$apikey->user_id}}/edit">here</a> to try again!</h5>
 
 @stop
--- resources/views/users/domains/edit_error.blade.php
+++ resources/views/users/domains/edit_error.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <h4>You can't use the same domainname!</h4>
+    <h4>This domainname is already in use!</h4>
     <h5>Click <a href="/domain/{{$domain->uuid}}/edit">here</a> to try again!</h5>
 
 @stop
