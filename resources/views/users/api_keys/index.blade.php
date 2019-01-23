697e1f8fdf7067d2423f5d71c3dddff0a75ca421
Unknown <m.koning02@student.alfa-college.nl>
2019-01-23 11:47:44 +0100

    Edit button style

--- resources/views/users/api_keys/index.blade.php
+++ resources/views/users/api_keys/index.blade.php
@@ -4,11 +4,12 @@
 
     <script>
         jQuery(document).ready(function(){
-            jQuery('.hideshow').on('click', function(event) {
-                jQuery(this).prev('.content').toggle();
+            jQuery('.btn-info').on('click', function(event) {
+                jQuery(this).next('.apikey').toggle();
             });
         });
     </script>
+
 
 
     <div class="col-sm6">
@@ -19,22 +20,27 @@
                 <thead>
                 <tr>
                     <th>Name Of API Key</th>
+                <th>
+                </th>
                 </thead>
                 <tbody>
                     @foreach($apikeys->where('user_id','=', Auth::user()->id) as $apikey)
                         <tr>
                             <td>
                                 <a href="/api/{{$apikey->uuid}}">{{$apikey->label}}</a>
-                                <div class="form-group" style="float:right">
-
+                            </td>
+                            <td>
+                                <div class="form-group" style="position: relative;">
                                 {!! Form::open(['action' => ['ApikeyController@destroy', $apikey->id], 'method' => 'POST', 'class' => 'pull-right']) !!}
                                 {{Form::hidden('_method', 'DELETE')}}
                                 {{Form::submit('Delete API Key', ['class' => 'btn btn-danger', 'onclick' => 'return confirm("Are you sure?")'])}}
                                 {!! Form::close() !!}
                             </div>
                         </td>
-                        <td><div id="hide" style="display: none;" class='content'>{{$apikey->verification_key}}</div>
-                            <input type='button' class='hideshow' value='Show Api key'></td>
+                        <td>
+                            <input type='button' class='btn btn-info' style="position: relative; width: 320px" value='Show Api key'>
+                            <p id="hide" style="display: none; position: relative;" class='apikey'>{{$apikey->verification_key}}</p>
+                        </td>
                     </tr>
                 @endforeach
                 {{$apikeys->links()}}
@@ -43,6 +49,6 @@
             @endif
         </table>
     </div>
-    <a href="/api/create" class="btn btn-info">Add a new API Key</a><br>
+    <a href="/api/create" class="btn btn-info" style="position: relative;" >Add a new API Key</a><br>
 
 @stop
